ababaca96e48741423ffd31cd71cf2015c749034
drwadu <dominik.rusovac@hotmail.com>
2021-08-20 13:37:11 +0200

    Increase cov.

--- src/cache.rs
+++ src/cache.rs
@@ -7,16 +7,33 @@ lazy_static::lazy_static! {
 
 #[derive(Debug)]
 pub struct Cache {
-    pub answer_set_counts: LruCache<String, usize>,
+    pub max_as_facets: LruCache<String, Vec<String>>,
+    pub min_as_facets: LruCache<String, Vec<String>>,
     pub max_fc_facets: LruCache<String, Vec<String>>,
     pub min_fc_facets: LruCache<String, Vec<String>>,
 }
 impl Cache {
     pub fn new(capacity: usize) -> Self {
-        Cache {
-            answer_set_counts: LruCache::with_hasher(capacity, DefaultHasher::default()),
+        Self {
+            max_as_facets: LruCache::with_hasher(capacity, DefaultHasher::default()),
+            min_as_facets: LruCache::with_hasher(capacity, DefaultHasher::default()),
             max_fc_facets: LruCache::with_hasher(capacity, DefaultHasher::default()),
             min_fc_facets: LruCache::with_hasher(capacity, DefaultHasher::default()),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn new() {
+        Cache::new(1_000);
+    }
+
+    #[test]
+    fn cache() {
+        assert!(CACHE.lock().is_ok());
     }
 }
